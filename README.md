7b278be7507204cb5f1a15d7f6bf4d475ff45733
Angelo Mondaini Calvão <oangelo@gmail.com>
2023-05-05 12:53:17 -0300

    Adding a basic working code

--- README.md
+++ README.md
@@ -5,3 +5,4 @@
 
 * Install the ESP8266 board
 * Use the following comand to build and upload from terminal: arduino --upload robot.ino --port /dev/ttyUSB*
+* One must use [ESP8266fs](https://github.com/esp8266/arduino-esp8266fs-plugin) to upload the html file on the folder data to the esp
--- robot/robot.ino
+++ robot/robot.ino
@@ -1,9 +1,8 @@
-#define ENA   14          // Enable/speed motors Right        GPIO14(D5)
-#define ENB   12          // Enable/speed motors Left         GPIO12(D6)
-#define IN_1  15          // L298N in1 motors Rightx          GPIO15(D8)
-#define IN_2  13          // L298N in2 motors Right           GPIO13(D7)
-#define IN_3  2           // L298N in3 motors Left            GPIO2(D4)
-#define IN_4  0           // L298N in4 motors Left            GPIO0(D3)
+//Pins definitions
+#define RightMotorSpeed 5
+#define RightMotorDir   0 
+#define LeftMotorSpeed  4
+#define LeftMotorDir    2
 
 #include <ESP8266WiFi.h>
 #include <WiFiClient.h> 
@@ -13,164 +12,186 @@ String command;             //String to store app command state.
 int speedCar = 800;         // 400 - 1023.
 int speed_Coeff = 3;
 
-const char* ssid = "Make DIY";
+//variáveis de controle dos motores
+int minRange = 300;
+int maxRange = 900;
+int minSpeed = 450;
+int maxSpeed = 1020;
+int noSpeed = 0;
+
+const char* ssid = "Robot";
 ESP8266WebServer server(80);
 
 void setup() {
- 
- pinMode(ENA, OUTPUT);
- pinMode(ENB, OUTPUT);  
- pinMode(IN_1, OUTPUT);
- pinMode(IN_2, OUTPUT);
- pinMode(IN_3, OUTPUT);
- pinMode(IN_4, OUTPUT); 
-  
-  Serial.begin(115200);
-  
-// Connecting WiFi
 
+  Serial.begin(115200);
+  Serial.println("Starting Setup");
+
+  if (!SPIFFS.begin()) {
+    Serial.println("Failed to mount file system");
+    return;
+  }
+
+  // setting motor pins as outputs
+  pinMode(RightMotorSpeed, OUTPUT);
+  pinMode(LeftMotorSpeed, OUTPUT);
+  pinMode(RightMotorDir, OUTPUT);
+  pinMode(LeftMotorDir, OUTPUT);
+  
+  // setting motor pins to LOW
+  digitalWrite(RightMotorSpeed, LOW);
+  digitalWrite(LeftMotorSpeed, LOW);
+  digitalWrite(RightMotorDir, HIGH);
+  digitalWrite(LeftMotorDir,HIGH);
+
+  // generating WiFi
   WiFi.mode(WIFI_AP);
   WiFi.softAP(ssid);
-
   IPAddress myIP = WiFi.softAPIP();
   Serial.print("AP IP address: ");
   Serial.println(myIP);
- 
- // Starting WEB-server 
-     server.on ( "/", HTTP_handleRoot );
-     server.onNotFound ( HTTP_handleRoot );
-     server.begin();    
+
+  // Starting WEB-server 
+  server.on ( "/", handleRoot );
+  server.onNotFound ( handleRoot );
+  server.begin();    
+
+  Serial.println("Finised Setup");
 }
 
 void goAhead(){ 
 
-      digitalWrite(IN_1, LOW);
-      digitalWrite(IN_2, HIGH);
-      analogWrite(ENA, speedCar);
+  Serial.println("goAhead");
 
-      digitalWrite(IN_3, LOW);
-      digitalWrite(IN_4, HIGH);
-      analogWrite(ENB, speedCar);
-  }
-
-void goBack(){ 
-
-      digitalWrite(IN_1, HIGH);
-      digitalWrite(IN_2, LOW);
-      analogWrite(ENA, speedCar);
-
-      digitalWrite(IN_3, HIGH);
-      digitalWrite(IN_4, LOW);
-      analogWrite(ENB, speedCar);
-  }
-
-void goRight(){ 
-
-      digitalWrite(IN_1, HIGH);
-      digitalWrite(IN_2, LOW);
-      analogWrite(ENA, speedCar);
-
-      digitalWrite(IN_3, LOW);
-      digitalWrite(IN_4, HIGH);
-      analogWrite(ENB, speedCar);
-  }
-
-void goLeft(){
-
-      digitalWrite(IN_1, LOW);
-      digitalWrite(IN_2, HIGH);
-      analogWrite(ENA, speedCar);
-
-      digitalWrite(IN_3, HIGH);
-      digitalWrite(IN_4, LOW);
-      analogWrite(ENB, speedCar);
-  }
-
-void goAheadRight(){
-      
-      digitalWrite(IN_1, LOW);
-      digitalWrite(IN_2, HIGH);
-      analogWrite(ENA, speedCar/speed_Coeff);
- 
-      digitalWrite(IN_3, LOW);
-      digitalWrite(IN_4, HIGH);
-      analogWrite(ENB, speedCar);
-   }
-
-void goAheadLeft(){
-      
-      digitalWrite(IN_1, LOW);
-      digitalWrite(IN_2, HIGH);
-      analogWrite(ENA, speedCar);
-
-      digitalWrite(IN_3, LOW);
-      digitalWrite(IN_4, HIGH);
-      analogWrite(ENB, speedCar/speed_Coeff);
-  }
-
-void goBackRight(){ 
-
-      digitalWrite(IN_1, HIGH);
-      digitalWrite(IN_2, LOW);
-      analogWrite(ENA, speedCar/speed_Coeff);
-
-      digitalWrite(IN_3, HIGH);
-      digitalWrite(IN_4, LOW);
-      analogWrite(ENB, speedCar);
-  }
-
-void goBackLeft(){ 
-
-      digitalWrite(IN_1, HIGH);
-      digitalWrite(IN_2, LOW);
-      analogWrite(ENA, speedCar);
-
-      digitalWrite(IN_3, HIGH);
-      digitalWrite(IN_4, LOW);
-      analogWrite(ENB, speedCar/speed_Coeff);
-  }
-
-void stopRobot(){  
-
-      digitalWrite(IN_1, LOW);
-      digitalWrite(IN_2, LOW);
-      analogWrite(ENA, speedCar);
-
-      digitalWrite(IN_3, LOW);
-      digitalWrite(IN_4, LOW);
-      analogWrite(ENB, speedCar);
- }
-
-void loop() {
-    server.handleClient();
-    
-      command = server.arg("State");
-      if (command == "F") goAhead();
-      else if (command == "B") goBack();
-      else if (command == "L") goLeft();
-      else if (command == "R") goRight();
-      else if (command == "I") goAheadRight();
-      else if (command == "G") goAheadLeft();
-      else if (command == "J") goBackRight();
-      else if (command == "H") goBackLeft();
-      else if (command == "0") speedCar = 400;
-      else if (command == "1") speedCar = 470;
-      else if (command == "2") speedCar = 540;
-      else if (command == "3") speedCar = 610;
-      else if (command == "4") speedCar = 680;
-      else if (command == "5") speedCar = 750;
-      else if (command == "6") speedCar = 820;
-      else if (command == "7") speedCar = 890;
-      else if (command == "8") speedCar = 960;
-      else if (command == "9") speedCar = 1023;
-      else if (command == "S") stopRobot();
+  digitalWrite(RightMotorDir,HIGH); 
+  digitalWrite(LeftMotorDir,HIGH);
+  analogWrite(RightMotorSpeed,maxSpeed);
+  analogWrite(LeftMotorSpeed,maxSpeed);
 }
 
-void HTTP_handleRoot(void) {
+void goBack(){ 
+  Serial.println("goBack");
+  digitalWrite(RightMotorDir,LOW);
+  digitalWrite(LeftMotorDir,LOW);
+  analogWrite(RightMotorSpeed,maxSpeed);
+  analogWrite(LeftMotorSpeed,maxSpeed);
 
-if( server.hasArg("State") ){
-       Serial.println(server.arg("State"));
+}
+
+void goRight(){ 
+  Serial.println("goRight");
+  digitalWrite(RightMotorDir,HIGH);
+  digitalWrite(LeftMotorDir,HIGH);
+  analogWrite(RightMotorSpeed,minSpeed);
+  analogWrite(LeftMotorSpeed,maxSpeed);
+
+
+}
+
+void goLeft(){
+  Serial.println("goLeft");
+  digitalWrite(RightMotorDir,HIGH);
+  digitalWrite(LeftMotorDir,HIGH);
+  analogWrite(RightMotorSpeed,maxSpeed);
+  analogWrite(LeftMotorSpeed,minSpeed);
+
+}
+
+void goAheadRight(){
+  Serial.println("goAheadRight");
+  digitalWrite(RightMotorDir,HIGH);
+  digitalWrite(LeftMotorDir,HIGH);
+  analogWrite(RightMotorSpeed,minSpeed);
+  analogWrite(LeftMotorSpeed,maxSpeed);
+
+}
+
+void goAheadLeft(){
+  Serial.println("goAheadLeft");
+  digitalWrite(RightMotorDir,LOW);
+  digitalWrite(LeftMotorDir,LOW);
+  analogWrite(RightMotorSpeed,maxSpeed);
+  analogWrite(LeftMotorSpeed,minSpeed);
+
+}
+
+void goBackRight(){ 
+  Serial.println("goBackRight");
+  digitalWrite(RightMotorDir,LOW);
+  digitalWrite(LeftMotorDir,LOW);
+  analogWrite(RightMotorSpeed,minSpeed);
+  analogWrite(LeftMotorSpeed,maxSpeed);
+
+}
+
+void goBackLeft(){ 
+  Serial.println("goBackLeft");
+  digitalWrite(RightMotorDir,LOW);
+  digitalWrite(LeftMotorDir,LOW);
+  analogWrite(RightMotorSpeed,maxSpeed);
+  analogWrite(LeftMotorSpeed,minSpeed);
+
+}
+
+void stopRobot(){  
+  Serial.println("stopRobot");
+  analogWrite(RightMotorSpeed,noSpeed);
+  analogWrite(LeftMotorSpeed,noSpeed);
+}
+
+void loop() {
+  server.handleClient();
+
+}
+
+void handleRoot(void) {
+
+  String message = "Parameters:";
+  for (int i = 0; i < server.args(); i++) {
+    message += "<br>"+ server.argName(i) + ": " + server.arg(i) + "\n";
   }
-  server.send ( 200, "text/html", "" );
+  Serial.println(message);
+
+  if( server.hasArg("State") ){
+    Serial.println("State:");
+    Serial.println(server.arg("State"));
+    command = server.arg("State");
+    switch (command[0]) {
+      case 'F': goAhead(); break;
+      case 'B': goBack(); break;
+      case 'L': goLeft(); break;
+      case 'R': goRight(); break;
+      case 'I': goAheadRight(); break;
+      case 'G': goAheadLeft(); break;
+      case 'J': goBackRight(); break;
+      case 'H': goBackLeft(); break;
+      case '0': speedCar = 400; break;
+      case '1': speedCar = 470; break;
+      case '2': speedCar = 540; break;
+      case '3': speedCar = 610; break;
+      case '4': speedCar = 680; break;
+      case '5': speedCar = 750; break;
+      case '6': speedCar = 820; break;
+      case '7': speedCar = 890; break;
+      case '8': speedCar = 960; break;
+      case '9': speedCar = 1023; break;
+      case 'S': stopRobot(); break;
+      default: break;
+    }
+  }
+  File file = SPIFFS.open("/page.html", "r");
+  
+  // Check if file opened successfully
+  if (!file) {
+    Serial.println("Failed to open file");
+    return;
+  }
+  
+  // Read file contents into a String variable
+  String html = file.readString();
+  
+  // Send HTTP response with file contents
+  server.send(200, "text/html", html);
+
   delay(1);
-  }
+}
